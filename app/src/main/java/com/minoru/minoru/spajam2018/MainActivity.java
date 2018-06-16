ecddb9c9d89a9cdbd3f17d0460b4ee9a8b54344d
ITO MINORU <mito@mikilab.doshisha.ac.jp>
2018-06-16 17:55:01 +0900

    retry

--- app/src/main/java/com/minoru/minoru/spajam2018/MainActivity.java
+++ app/src/main/java/com/minoru/minoru/spajam2018/MainActivity.java
@@ -1,13 +1,28 @@
 package com.minoru.minoru.spajam2018;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaManager Manager = new MediaManager();
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer.create(this,R.raw.hakucyou);
+
+//        Manager.setMyActivity(this);
+        Manager.setMyActivity(this,mediaPlayer);
+//        Manager.getMaxVol();
+        Manager.setVolume(14);
+        try{
+            Manager.prepare();
+        }catch( Exception e ){ }
+        Manager.playSound();
     }
 }
